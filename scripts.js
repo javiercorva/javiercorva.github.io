9e4e33b76184e9c1b44c66bae00f20b0d37fe344
Javier Corvalán <127362678+javiercorva@users.noreply.github.com>
2023-03-08 23:28:03 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,20 @@
+
+document.getElementById("boton-celu").onclick = function () {
+    console.log("Capturamos el evento click en boton-celu");
+    document.getElementById("celu").innerHTML = "15-1234-5678";
+}
+
+document.getElementById("boton-email").onclick = function () {
+    console.log("Capturamos el evento click en boton-email");
+    document.getElementById("email").innerHTML ="lorem@ipsum.com.ar";
+}
+
+document.getElementById("boton-instagram").onclick = function () {
+    console.log("Capturamos el evento click en boton-instagram");
+    document.getElementById("instagram").innerHTML ="/lorem_ipsum2023";
+}
+
+document.getElementById("boton-face").onclick = function () {
+    console.log("Capturamos el evento click en boton-face");
+    document.getElementById("face").innerHTML ="/lorem_ipsum";
+}
